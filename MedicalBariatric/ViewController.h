68f9ca4220a5afc2c02c03c1b8381a01749ef292
g1sus <proyectos_technology@hotmail.com>
2015-03-10 15:46:22 -0600

    Initial Commit

---
+++ MedicalBariatric/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MedicalBariatric
+//
+//  Created by Jesus Antonio Hernandez Mendez on 10/03/15.
+//  Copyright (c) 2015 BTICs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MedicalBariatric/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MedicalBariatric
+//
+//  Created by Jesus Antonio Hernandez Mendez on 10/03/15.
+//  Copyright (c) 2015 BTICs. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
